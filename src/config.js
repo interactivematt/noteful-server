693d227ba2a50e0a69bcdc5743567a2517bfba25
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-23 21:25:51 -0600

    Trying this

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || '8000',
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
